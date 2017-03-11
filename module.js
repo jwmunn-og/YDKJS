59b8aec92915bafca831c68bbdd31afd18db2242
jwmunn <contact@justinmunn.co>
2017-03-11 12:23:39 -0800

    Module function receiving parameters.

--- module.js
+++ module.js
@@ -1,22 +1,46 @@
-function CoolModule(){
-  var something = "cool",
-      another  = [1,2,3];
+// --------------------------
+// Module Pattern Example
+// --------------------------
 
-  function doSomething(){
-    console.log(something);
-  }
+// function CoolModule(){
+//   var something = "cool",
+//       another  = [1,2,3];
 
-  function doAnother(){
-    console.log(another.join("!"));
+//   function doSomething(){
+//     console.log(something);
+//   }
+
+//   function doAnother(){
+//     console.log(another.join("!"));
+//   }
+
+//   return {
+//     doSomething: doSomething,
+//     doAnother: doAnother
+//   };
+// }
+
+// var foo = CoolModule();
+
+// foo.doSomething(); // cool
+// foo.doAnother(); // 1 ! 2 ! 3
+
+// --------------------------
+// Parameter Module Example
+// --------------------------
+
+function CoolModule(id){
+  function identify(){
+    console.log(id);
   }
 
   return {
-    doSomething: doSomething,
-    doAnother: doAnother
+    identify: identify
   };
 }
 
-var foo = CoolModule();
+foo1 = CoolModule("foo 1");
+foo2 = CoolModule("foo 2");
 
-foo.doSomething(); // cool
-foo.doAnother(); // 1 ! 2 ! 3
+foo1.identify(); // foo 1
+foo2.identify(); // foo 2
